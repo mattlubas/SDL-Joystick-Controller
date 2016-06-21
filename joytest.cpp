3d379e9f6e5894374877f0f4636e83eb4d83c964
mattlubas <lubasm18@mail.wlu.edu>
2016-06-21 09:52:30 -0400

    Added Button Tracking to controller

--- joytest.cpp
+++ joytest.cpp
@@ -19,6 +19,7 @@ int main(int argc, char *argv[])
   if (SDL_Init(SDL_INIT_JOYSTICK))
     error("Failed to initialize SDL");
 
+  
   //Initializes Joystick in SDL
   SDL_Init(SDL_INIT_JOYSTICK);
 
@@ -48,44 +49,22 @@ int main(int argc, char *argv[])
 	   printf("Axis:  %d\t",event.jaxis.axis);
 	   printf("Value: %d\n",event.jaxis.value);
     	
- 
+	case SDL_JOYBUTTONDOWN:
+
+	   printf("Button: %hhu\t",event.jbutton.button);
+	   printf("State: %hhu\n",event.jbutton.state);
+	   
+	case SDL_JOYBUTTONUP:
+
+	   printf("Button: %hhu\t",event.jbutton.button);
+	   printf("State: %hhu\n",event.jbutton.state); 
 	}
-
-
-
-    /*
-    // SDL_JoyAxisEvent(which,axis, value) Axis range from -32768 to 32767
-    // SDL_JoyAxisEvent(value_0);
-    // SDL_JoyAxisEvent(0,1,value_1);
-    // SDL_JoyAxisEvent(0,2,value_2);
-    // SDL_JoyAxisEvent(0,3,value_3);
-
-    short axis_0 = value_0;
-    int axis_1 = value_1;
-    int axis_2 = value_2;
-    int axis_3 = value_3;
-    */
-
-    // Sint16 axis_0, axis_1, axis_2, axis_3, axis_4;
-    /*
-    axis_0 = SDL_JoystickGetAxis(Joy,0);
-    axis_1 = SDL_JoystickGetAxis(Joy,1);
-    axis_2 = SDL_JoystickGetAxis(Joy,2);
-    axis_3 = SDL_JoystickGetAxis(Joy,3);
-    */
-    /* 
-    printf("Axis_0: %hd\t",axis_0);
-    printf("Axis_1: %hd\t",axis_1);
-    printf("Axis_2: %hd\t",axis_2);
-    printf("Axis_3: %hd\n",axis_3);
-    */
-
 
     //Sleep program for 0.1 Seconds
     usleep(100000);
  
     //Updates Controller
-    int SDL_JoystickEventState(SDL_ENABLE);
+    //int SDL_JoystickEventState(SDL_ENABLE);
 
     while( SDL_PollEvent(&event ) )
     {
